beb35ac1cec2297ce09054b263f43f3aaeae5bbe
paulosaure <lavoine@polytech.unice.fr>
2014-12-19 16:57:04 +0100

    Ajout Server

---
+++ C#/PaintSurface/SurfaceWindow1.xaml.cs
@@ -0,0 +1,152 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using Microsoft.Surface;
+using Microsoft.Surface.Presentation;
+using Microsoft.Surface.Presentation.Controls;
+using Microsoft.Surface.Presentation.Input;
+using System.Diagnostics;
+using System.Net;
+using System.IO;
+
+namespace PaintSurface
+{
+    /// <summary>
+    /// Interaction logic for SurfaceWindow1.xaml
+    /// </summary>
+    public partial class SurfaceWindow1 : SurfaceWindow
+    {
+        private Process _serverProcess;
+
+        private int _serverPort = 8080;
+
+        private SocketManager _sm;
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public SurfaceWindow1()
+        {
+            InitializeComponent();
+
+            // Add handlers for window availability events
+            AddWindowAvailabilityHandlers();
+
+            this._startServer();
+
+            string localIp = this._getLocalIPAddress();
+
+            Console.WriteLine("http://" + localIp + ":" + this._serverPort.ToString());
+
+            this._sm = new SocketManager("http://localhost:" + this._serverPort.ToString());
+
+        }
+
+        private void _startServer()
+        {
+            this._serverProcess = new Process();
+            this._serverProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            this._serverProcess.StartInfo.CreateNoWindow = false;
+            this._serverProcess.StartInfo.UseShellExecute = false;
+            this._serverProcess.StartInfo.FileName = "cmd.exe";
+            this._serverProcess.StartInfo.Arguments = "/c cd ../../../PaintServer/PaintServer/ & node PaintServer.js";
+            this._serverProcess.EnableRaisingEvents = true;
+            this._serverProcess.Start();
+        }
+
+        private string _getLocalIPAddress()
+        {
+            IPHostEntry host;
+            string localIP = "";
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    localIP = ip.ToString();
+                    break;
+                }
+            }
+            return localIP;
+        }
+
+        /// <summary>
+        /// Occurs when the window is about to close. 
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+
+            this._serverProcess.CloseMainWindow();
+
+            // Remove handlers for window availability events
+            RemoveWindowAvailabilityHandlers();
+        }
+
+        /// <summary>
+        /// Adds handlers for window availability events.
+        /// </summary>
+        private void AddWindowAvailabilityHandlers()
+        {
+            // Subscribe to surface window availability events
+            ApplicationServices.WindowInteractive += OnWindowInteractive;
+            ApplicationServices.WindowNoninteractive += OnWindowNoninteractive;
+            ApplicationServices.WindowUnavailable += OnWindowUnavailable;
+        }
+
+        /// <summary>
+        /// Removes handlers for window availability events.
+        /// </summary>
+        private void RemoveWindowAvailabilityHandlers()
+        {
+            // Unsubscribe from surface window availability events
+            ApplicationServices.WindowInteractive -= OnWindowInteractive;
+            ApplicationServices.WindowNoninteractive -= OnWindowNoninteractive;
+            ApplicationServices.WindowUnavailable -= OnWindowUnavailable;
+        }
+
+        /// <summary>
+        /// This is called when the user can interact with the application's window.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnWindowInteractive(object sender, EventArgs e)
+        {
+            //TODO: enable audio, animations here
+        }
+
+        /// <summary>
+        /// This is called when the user can see but not interact with the application's window.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnWindowNoninteractive(object sender, EventArgs e)
+        {
+            //TODO: Disable audio here if it is enabled
+
+            //TODO: optionally enable animations here
+        }
+
+        /// <summary>
+        /// This is called when the application's window is not visible or interactive.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnWindowUnavailable(object sender, EventArgs e)
+        {
+            //TODO: disable audio, animations here
+        }
+    }
+}
